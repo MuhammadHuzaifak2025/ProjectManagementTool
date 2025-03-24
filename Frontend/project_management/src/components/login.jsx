90359bd7bccab0107b8a1e54978cdc54d7e5059f
Muhammad Huzaifa <mhuzaifa91@gmail.com>
2025-03-25 01:20:50 +0500

    Add utility functions, button component, and update configuration files; enhance styling and theming

--- Frontend/project_management/src/components/login.jsx
+++ Frontend/project_management/src/components/login.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { Button } from "@/components/ui/button"
 const login = () => {
-  
+
   return (
-    <div>login</div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Login</Button>
+    </div>
   )
 }
 
